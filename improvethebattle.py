a0d86c19e81d5b237c45668f240e30a88eec6621
Chunyao Liu <129890897+cliu68@users.noreply.github.com>
2023-05-21 19:41:59 -0700

    Update improvethebattle.py

--- improvethebattle.py
+++ improvethebattle.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 mongoClient = MongoClient("mongodb://localhost/pokemon")
 pokemonDB = mongoClient['pokemondb']
-pokemonColl = pokemonDB['pokemon_data']
+pokemonColl = pokemonDB['pokemonk_data']
 
 def fetch(pokemonid):
     return pokemonColl.find_one({"pokedex_number":pokemonid})
